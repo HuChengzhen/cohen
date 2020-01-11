0eb1538ca79c71bbc700d66e67bf855c70edf575
Hu Chengzhen <huchengzhen@gmail.com>
2020-01-11 15:06:39 +0800

    first commit

---
+++ src/main/java/com/huchengzhen/cohen/CohenApplication.java
@@ -0,0 +1,13 @@
+package com.huchengzhen.cohen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CohenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CohenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huchengzhen/cohen/CohenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huchengzhen.cohen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CohenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
