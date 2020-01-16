bb592a3f3e6d9d37caad2ec8dfc7065e9f8440bc
Hu Chengzhen <huchengzhen@gmail.com>
2020-01-17 02:17:36 +0800

    fail test.

--- src/test/java/com/huchengzhen/cohen/CohenApplicationTests.java
+++ src/test/java/com/huchengzhen/cohen/CohenApplicationTests.java
@@ -1,5 +1,6 @@
 package com.huchengzhen.cohen;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,5 +9,7 @@ class CohenApplicationTests {
 
     @Test
     void contextLoads() {
+        // fail
+        Assertions.assertThat(1).isEqualTo(0);
     }
 }
