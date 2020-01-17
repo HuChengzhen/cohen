c9765caf06b48633c6858a1dff40af47ae816794
Hu Chengzhen <huchengzhen@gmail.com>
2020-01-17 08:47:32 +0800

    docker test build.

--- sql/insert.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `user`
-VALUES (null, 'hcz', 'hcz1995@qq.com', '$2a$10$qTU7t4u3Si9AwusJTGFAZ.LdfYwASPxI65spRsoMkXDgHSfJrQekO',
-        '2020-01-13 20:24:35', '2020-01-15 15:50:54', 1, 'ROLE_USER,ROLE_ADMIN'),
-       (13, 'cohen', 'cohen@gmail.com', '$2a$10$fBzyNHG2FkmYmTpki4Fim.CJrWMYKIhBfMnzXiFrkEFGVVNsHwwni',
-        '2020-01-14 11:05:51', '2020-01-14 20:28:00', 1, 'ROLE_USER');
