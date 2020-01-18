9f29b143f3c0500c9ecd8601fc47345161842319
Hu Chengzhen <huchengzhen@gmail.com>
2020-01-18 15:31:17 +0800

    rm container after create.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,9 @@
 docker-compose stop
-docker container stop cohen-mysql
-docker container rm cohen-mysql
 docker build -f DockerfileMysql -t cohen-mysql:latest .
 docker run --name cohen-mysql -v ~/cohen/db-data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=ASD2100150 -d cohen-mysql:latest
 read -p "Pause Time 5 seconds" -t 5
 docker exec -i cohen-mysql sh -c 'exec mysql -uroot -pASD2100150' <sql/update.sql
 docker stop cohen-mysql
+docker container rm cohen-mysql
 docker-compose build
 docker-compose up --force-recreate -d
