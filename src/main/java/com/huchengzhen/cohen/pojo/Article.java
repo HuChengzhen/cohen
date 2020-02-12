c08f3693af69aab04dd6e448cf5653db38cd59ec
Hu Chengzhen <huchengzhen@gmail.com>
2020-02-12 19:43:46 +0800

    fix

--- src/main/java/com/huchengzhen/cohen/pojo/Article.java
+++ src/main/java/com/huchengzhen/cohen/pojo/Article.java
@@ -3,16 +3,21 @@ package com.huchengzhen.cohen.pojo;
 import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class Article {
+    @ApiParam(hidden = true)
     private Integer id;
     private String title;
     private String article;
+    @ApiParam(hidden = true)
     private Integer authorId;
+    @ApiParam(hidden = true)
     private Date uploadedDate;
+    @ApiParam(hidden = true)
     private Date editDate;
 }
--- src/main/java/com/huchengzhen/cohen/pojo/Comment.java
+++ src/main/java/com/huchengzhen/cohen/pojo/Comment.java
@@ -1,15 +1,20 @@
 package com.huchengzhen.cohen.pojo;
 
+import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class Comment {
+    @ApiParam(hidden = true)
     private Integer id;
+    @ApiParam(hidden = true)
     private Integer userId;
     private Integer articleId;
     private String comment;
+    @ApiParam(hidden = true)
     private Date commentDate;
+    @ApiParam(hidden = true)
     private User user;
 }
