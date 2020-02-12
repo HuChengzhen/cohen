815412e38f72e98d72e986449c50241a913f7f45
Hu Chengzhen <huchengzhen@gmail.com>
2020-02-12 19:40:06 +0800

    api hidden

--- src/main/java/com/huchengzhen/cohen/pojo/UserFollow.java
+++ src/main/java/com/huchengzhen/cohen/pojo/UserFollow.java
@@ -1,6 +1,9 @@
 package com.huchengzhen.cohen.pojo;
 
+import io.swagger.annotations.ApiParam;
+
 public class UserFollow {
+    @ApiParam(hidden = true)
     private Integer id;
     private Integer follower;
     private Integer followed;
